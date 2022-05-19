5776d8ae0aad3241b1f81b661d70772d2edd91f8
huyphamfc <huyphamfc@gmail.com>
2022-05-19 17:26:53 +0700

    Find Disappeared Numbers - Solution 2

---
+++ 18--Find-All-Numbers-Disappeared-in-an-Array/README.md
@@ -0,0 +1,21 @@
+# Find All Numbers Disappeared in an Array
+
+Give an array `arr` of `n` integers where `arr[i]` is in the range `[1,n]`, return an array of all the integers in the range `[1,n]` that do not appear in `arr`.
+
+Example 1:
+
+```
+Input: arr = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+```
+
+Example 2:
+
+```
+Input: arr = [1,1]
+Output: [2]
+```
+
+&copy; [LeetCode](https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3270/)
+
+Happy Coding :))
--- 18--Find-All-Numbers-Disappeared-in-an-Array/script.js
+++ 18--Find-All-Numbers-Disappeared-in-an-Array/script.js
@@ -7,12 +7,15 @@ Author: huyphamfc
 Solution 1:
     Runtime: 5849 ms
     Memory Usage: 49.8 MB
+Solution 2:
+    Runtime: 7457 ms
+    Memory Usage: 54.1 MB
 */
 
 'use strict'
 
-const arr = [4, 3, 2, 7, 8, 2, 3, 1];
-// const arr = [1, 1];
+// const arr = [4, 3, 2, 7, 8, 2, 3, 1];
+const arr = [1, 1];
 
 const findDisappearedNumbers = function (arr) {
     const disappearedNumbers = [];
@@ -29,4 +32,21 @@ const findDisappearedNumbers = function (arr) {
     return disappearedNumbers;
 }
 
-console.log(findDisappearedNumbers(arr));
+const findDisappearedNumbers_2 = function (arr) {
+    const disappearedNumbers = [];
+    const uniqueArr = [...new Set(arr)];
+    for (let i = 1; i <= arr.length; i++) {
+        let disappeared = true;
+        for (let j = 0; j < uniqueArr.length; j++) {
+            if (uniqueArr[j] === i) {
+                disappeared = false;
+                break;
+            }
+        }
+        if (disappeared) disappearedNumbers.push(i);
+    }
+    return disappearedNumbers;
+}
+
+// console.log(findDisappearedNumbers(arr));
+console.log(findDisappearedNumbers_2(arr));
