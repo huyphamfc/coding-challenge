508f0621d20ab63058101765cae0c9bdac9af7bf
huyphamfc <huyphamfc@gmail.com>
2022-05-19 17:05:59 +0700

    Find Disappeared Numbers - Solution 1

---
+++ 18--Find-All-Numbers-Disappeared-in-an-Array/script.js
@@ -0,0 +1,32 @@
+/*
+Purpose: find all numbers disappeared in an array
+Create on May 19, 2022
+Author: huyphamfc
+
+33/33 test cases passed
+Solution 1:
+    Runtime: 5849 ms
+    Memory Usage: 49.8 MB
+*/
+
+'use strict'
+
+const arr = [4, 3, 2, 7, 8, 2, 3, 1];
+// const arr = [1, 1];
+
+const findDisappearedNumbers = function (arr) {
+    const disappearedNumbers = [];
+    for (let i = 1; i <= arr.length; i++) {
+        let disappeared = true;
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] === i) {
+                disappeared = false;
+                break;
+            }
+        }
+        if (disappeared) disappearedNumbers.push(i);
+    }
+    return disappearedNumbers;
+}
+
+console.log(findDisappearedNumbers(arr));
